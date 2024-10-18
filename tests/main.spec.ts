6f1d7c8ecdf465f6b38aa6f6ec273e340ba6a1de
unknown <rommelarin@gmail.com>
2024-10-18 16:42:13 +0800

    newchnagesforMainsSPec

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,5 +1,5 @@
 import {expect , test} from '@playwright/test';
-import {PlaywrightProjects}  from '../page/Login'
+import {PlaywrightProjects}  from '../page/Login';
 
 
 test ("MyFirst Programming", async ({page}) =>{
@@ -7,6 +7,8 @@ test ("MyFirst Programming", async ({page}) =>{
     const playWrightLogin = new PlaywrightProjects (page);
     await playWrightLogin.goTo ();
     await playWrightLogin.loginPageStandard ();
+
+    
 
 
      
