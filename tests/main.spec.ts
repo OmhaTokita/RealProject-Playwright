bf9812b17102c3a716f9199d770160e2dba622f7
unknown <rommelarin@gmail.com>
2024-10-18 16:26:55 +0800

    adding Page's for POM

---
+++ tests/main.spec.ts
@@ -0,0 +1,15 @@
+import {expect , test} from '@playwright/test';
+import {PlaywrightProjects}  from '../page/Login'
+
+
+test ("MyFirst Programming", async ({page}) =>{
+
+    const playWrightLogin = new PlaywrightProjects (page);
+    await playWrightLogin.goTo ();
+    await playWrightLogin.loginPageStandard ();
+
+
+     
+
+
+})
