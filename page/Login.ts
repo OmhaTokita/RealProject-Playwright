8768642ff76bc790192d40d5d944e7239ddb5f42
unknown <rommelarin@gmail.com>
2024-10-18 16:48:49 +0800

    adding //

--- page/Login.ts
+++ page/Login.ts
@@ -20,7 +20,7 @@ import { expect, type Locator, type Page } from '@playwright/test';
         }
 
     async goTo (){
-        await this.page.goto ("https://www.saucedemo.com/");
+        await this.page.goto ("https://www.saucedemo.com////");
         await this.page.waitForLoadState ("networkidle");
         
     }
